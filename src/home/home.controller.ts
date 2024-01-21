2330a82025ff6161743e30827be57679a3f01900
Antonbinom <protest_the_hero@mail.ru>
2024-01-21 12:47:44 +0530

    add: home service and endpoints

---
+++ src/home/home.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { HomeService } from './home.service';
+import { CreateHomeDto, HomeResponseDto, UpdateHomeDto } from './dto/home.dto';
+import { PropertyType } from '@prisma/client';
+
+@Controller('home')
+export class HomeController {
+  constructor(private readonly homeService: HomeService) {}
+
+  @Get()
+  getHomes(
+    @Query('city') city?: string,
+    @Query('minPrice') minPrice?: string,
+    @Query('maxPrice') maxPrice?: string,
+    @Query('propertyType') propertyType?: PropertyType,
+  ): Promise<HomeResponseDto[]> {
+    const filters = {
+      ...(city && { city }),
+      ...((minPrice || maxPrice) && {
+        ...(minPrice && { gte: parseFloat(minPrice) }),
+        ...(maxPrice && { lte: parseFloat(maxPrice) }),
+      }),
+      price: {
+        gte: 1000000,
+        lte: 2000000,
+      },
+      ...(propertyType && { propertyType }),
+    };
+
+    return this.homeService.getHomes(filters);
+  }
+
+  @Get(':id')
+  getHome(@Param('id', ParseIntPipe) id: number) {
+    return this.homeService.getHomeById(id);
+  }
+
+  @Post()
+  createHome(@Body() body: CreateHomeDto) {
+    return this.homeService.createHome(body);
+  }
+
+  @Put(':id')
+  updateHome(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: UpdateHomeDto,
+  ) {
+    return this.homeService.updateHomeById(id, body);
+  }
+
+  @Delete(':id')
+  deleteHome(@Param('id', ParseIntPipe) id: number) {
+    return this.homeService.deleteHomeById(id);
+  }
+}
